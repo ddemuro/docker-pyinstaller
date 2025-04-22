ff42cf432d09a3b6a4b692e869c0711b3fb2de88
Derek Demuro <ddemuro@gmail.com>
2025-04-22 16:28:31 -0400

    Add a way to easily update moving forward

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Docker flags
-FLAGS_NOCACHE="--no-cache --progress=plain"
-FLAGS="--progress=plain"
-
+#FLAGS_NOCACHE="--no-cache --progress=plain"
+#FLAGS="--progress=plain"
+FLAGS="--quiet"
+TODAY=$(date +"%Y%m%d")
 FLAGTOUSE=$FLAGS
 
 function printSuccessOrFail {
@@ -17,85 +18,74 @@ function printSuccessOrFail {
 
 echo "Starting build process..."
 
-docker build $FLAGTOUSE -f Dockerfile-py2-amd64 -t ddemuro/pyinstaller:py2-amd64 .
-printSuccessOrFail
+docker build $FLAGTOUSE -f Dockerfile-py2-amd64 -t ddemuro/pyinstaller:py2-amd64-$TODAY -t ddemuro/pyinstaller:py2-amd64 . &
+docker build $FLAGTOUSE -f Dockerfile-py2-win32 -t ddemuro/pyinstaller:py2-win32-$TODAY -t ddemuro/pyinstaller:py2-win32 . &
+time wait
 
-docker build $FLAGTOUSE -f Dockerfile-py2-win32 -t ddemuro/pyinstaller:py2-win32 .
-printSuccessOrFail
+VERSIONS="3.9.9 3.12.3"
 
-docker build $FLAGTOUSE -f Dockerfile-py3-amd64 -t ddemuro/pyinstaller:py3-amd64 .
-printSuccessOrFail
+for VERSION in $VERSIONS; do
+    echo "Building py3-$VERSION..."
+    docker build $FLAGTOUSE -f Dockerfile-py3-amd64-$VERSION -t ddemuro/pyinstaller:py3-amd64-$VERSION -t ddemuro/pyinstaller:py3-amd64-$VERSION-$TODAY . &
 
-docker build $FLAGTOUSE -f Dockerfile-py3-i386 -t ddemuro/pyinstaller:py3-i386 .
-printSuccessOrFail
+    docker build $FLAGTOUSE -f Dockerfile-py3-i386-$VERSION -t ddemuro/pyinstaller:py3-i386-$VERSION -t ddemuro/pyinstaller:py3-i386-$VERSION-$TODAY . &
 
-docker build $FLAGTOUSE -f Dockerfile-py3-win32 -t ddemuro/pyinstaller:py3-win32 .
-printSuccessOrFail
+    docker build $FLAGTOUSE -f Dockerfile-py3-win32-$VERSION -t ddemuro/pyinstaller:py3-win32-$VERSION -t ddemuro/pyinstaller:py3-win32-$VERSION-$TODAY . &
 
-docker build $FLAGTOUSE -f Dockerfile-py3-win64 -t ddemuro/pyinstaller:py3-win64 .
-printSuccessOrFail
+    docker build $FLAGTOUSE -f Dockerfile-py3-win64-$VERSION -t ddemuro/pyinstaller:py3-win64-$VERSION -t ddemuro/pyinstaller:py3-win64-$VERSION-$TODAY . &
+done
 
-echo "Build process completed."
+time wait
+
+# echo "Build process completed."
  
-# Push images to Docker Hub
-# Grab today's date
-TODAY=$(date +"%Y%m%d")
+# # Push images to Docker Hub
+# # Grab today's date
+# 
 
-# Tag the images with the date
-docker tag ddemuro/pyinstaller:py2-amd64 ddemuro/pyinstaller:py2-amd64-$TODAY
-docker tag ddemuro/pyinstaller:py2-win32 ddemuro/pyinstaller:py2-win32-$TODAY
-docker tag ddemuro/pyinstaller:py3-amd64 ddemuro/pyinstaller:py3-amd64-$TODAY
-docker tag ddemuro/pyinstaller:py3-i386 ddemuro/pyinstaller:py3-i386-$TODAY
-docker tag ddemuro/pyinstaller:py3-win32 ddemuro/pyinstaller:py3-win32-$TODAY
-docker tag ddemuro/pyinstaller:py3-win64 ddemuro/pyinstaller:py3-win64-$TODAY
-
-# Push the images to Docker Hub
-# Push with the date
-docker push ddemuro/pyinstaller:py2-amd64-$TODAY
-docker push ddemuro/pyinstaller:py2-win32-$TODAY
-docker push ddemuro/pyinstaller:py3-amd64-$TODAY
-docker push ddemuro/pyinstaller:py3-i386-$TODAY
-docker push ddemuro/pyinstaller:py3-win32-$TODAY
-docker push ddemuro/pyinstaller:py3-win64-$TODAY
-
-# Refresh latest tags
-docker push ddemuro/pyinstaller:py2-amd64
-docker push ddemuro/pyinstaller:py2-win32
-docker push ddemuro/pyinstaller:py3-amd64
-docker push ddemuro/pyinstaller:py3-i386
-docker push ddemuro/pyinstaller:py3-win32
-docker push ddemuro/pyinstaller:py3-win64
-
-# Tag for private repo dkrhub.takelan.com/ddemuro
-docker tag ddemuro/pyinstaller:py2-amd64 dkrhub.takelan.com/ddemuro/pyinstaller:py2-amd64
-docker tag ddemuro/pyinstaller:py2-win32 dkrhub.takelan.com/ddemuro/pyinstaller:py2-win32
-docker tag ddemuro/pyinstaller:py3-amd64 dkrhub.takelan.com/ddemuro/pyinstaller:py3-amd64
-docker tag ddemuro/pyinstaller:py3-i386 dkrhub.takelan.com/ddemuro/pyinstaller:py3-i386
-docker tag ddemuro/pyinstaller:py3-win32 dkrhub.takelan.com/ddemuro/pyinstaller:py3-win32
-docker tag ddemuro/pyinstaller:py3-win64 dkrhub.takelan.com/ddemuro/pyinstaller:py3-win64
-
-# Private repo today tag
-docker tag ddemuro/pyinstaller:py2-amd64 dkrhub.takelan.com/ddemuro/pyinstaller:py2-amd64-$TODAY
-docker tag ddemuro/pyinstaller:py2-win32 dkrhub.takelan.com/ddemuro/pyinstaller:py2-win32-$TODAY
-docker tag ddemuro/pyinstaller:py3-amd64 dkrhub.takelan.com/ddemuro/pyinstaller:py3-amd64-$TODAY
-docker tag ddemuro/pyinstaller:py3-i386 dkrhub.takelan.com/ddemuro/pyinstaller:py3-i386-$TODAY
-docker tag ddemuro/pyinstaller:py3-win32 dkrhub.takelan.com/ddemuro/pyinstaller:py3-win32-$TODAY
-docker tag ddemuro/pyinstaller:py3-win64 dkrhub.takelan.com/ddemuro/pyinstaller:py3-win64-$TODAY
-
-# Push to private repo
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py2-amd64
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py2-win32
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-amd64
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-i386
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-win32
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-win64
-
-# Push to private repo with today tag
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py2-amd64-$TODAY
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py2-win32-$TODAY
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-amd64-$TODAY
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-i386-$TODAY
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-win32-$TODAY
-docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-win64-$TODAY
-
+# # Push the images to Docker Hub
+# # Push with the date
+docker push ddemuro/pyinstaller:py2-amd64 &
+docker push ddemuro/pyinstaller:py2-win32 &
+docker push ddemuro/pyinstaller:py2-amd64-$TODAY &
+docker push ddemuro/pyinstaller:py2-win32-$TODAY &
+time wait
 echo "Images pushed to Docker Hub."
+
+for VERSION in $VERSIONS; do
+    docker push ddemuro/pyinstaller:py3-amd64-$VERSION &
+    docker push ddemuro/pyinstaller:py3-i386-$VERSION &
+    docker push ddemuro/pyinstaller:py3-win32-$VERSION &
+    docker push ddemuro/pyinstaller:py3-win64-$VERSION &
+
+    docker push ddemuro/pyinstaller:py3-amd64-$VERSION-$TODAY &
+    docker push ddemuro/pyinstaller:py3-i386-$VERSION-$TODAY &
+    docker push ddemuro/pyinstaller:py3-win32-$VERSION-$TODAY &
+    docker push ddemuro/pyinstaller:py3-win64-$VERSION-$TODAY &
+done
+time wait
+echo "Images pushed to Docker Hub."
+
+
+docker push dkrhub.takelan.com/ddemuro/pyinstaller:py2-amd64 &
+docker push dkrhub.takelan.com/ddemuro/pyinstaller:py2-win32 &
+docker push dkrhub.takelan.com/ddemuro/pyinstaller:py2-amd64-$TODAY &
+docker push dkrhub.takelan.com/ddemuro/pyinstaller:py2-win32-$TODAY &
+time wait
+echo "Images pushed to private repo."
+
+for VERSION in $VERSIONS; do
+    docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-amd64-$VERSION &
+    docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-i386-$VERSION &
+    docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-win32-$VERSION &
+    docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-win64-$VERSION &
+
+    docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-amd64-$VERSION-$TODAY &
+    docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-i386-$VERSION-$TODAY &
+    docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-win32-$VERSION-$TODAY &
+    docker push dkrhub.takelan.com/ddemuro/pyinstaller:py3-win64-$VERSION-$TODAY &
+done
+time wait
+echo "Images pushed to private repo."
+
+echo Done
